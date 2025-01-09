edefa769dc71b39d85f73383800d289d49a3536d
AlshaimaaAbdElnaeem <alshaimaaabdelnaeem@gmail.com>
2025-01-09 03:05:04 +0200

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:book_mart/core/errors/failures.dart';
+import 'package:book_mart/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+ Future<Either <Failures,List<BookModel>>> fetchBestSellerBooks();
+   Future<Either <Failures,List<BookModel>>>fetchFeaturedBooks();
+}
